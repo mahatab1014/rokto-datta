96e18de41eca42cad04352e228147d64883ae296
DeveloperMahTab <mahtabhossain1014@gmail.com>
2023-12-31 22:42:00 +0600

    init basic server

---
+++ server/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+// CORS Middleware
+const corsOptions = {
+  origin: ["*"],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// MongoDB Configuration
+const client = new MongoClient(process.env.MONGO_DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const database = client.db("RoktoDatta_DB");
+    const postCollection = database.collection("donation_posts");
+
+
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
