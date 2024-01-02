d38ef1f84933262da0ed822b5882ffdbc1bc6730
DeveloperMahTab <mahtabhossain1014@gmail.com>
2024-01-02 15:26:04 +0600

    add skeleton when post details is loading

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -10,6 +10,7 @@ import { TbUrgent } from "react-icons/tb";
 
 import { Avatar } from "@mui/material";
 import DomHead from "../../components/shared/DomHead/DomHead";
+import Skeleton from "react-loading-skeleton";
 
 const PostDetails = () => {
   const { id } = useParams();
@@ -17,7 +18,6 @@ const PostDetails = () => {
   const { singlePostData, singlePostDataLoading } = useSinglePostData(id);
 
   const data = singlePostData?.data;
-  console.log(window.location);
 
   // TIME ZONE
   const postedTimeUTC = moment.utc(data?.posted_at);
@@ -31,6 +31,7 @@ const PostDetails = () => {
   const hours = duration.asHours();
   const days = duration.asDays();
   const weeks = duration.asWeeks();
+  const years = duration.asYears();
 
   return (
     <>
@@ -46,10 +47,37 @@ const PostDetails = () => {
             <>
               {singlePostDataLoading ? (
                 <>
-                  <h2 className="text-4xl">Loading</h2>
+                  <article className="">
+                    <div className="flex flex-col md:flex-row gap-5">
+                      <div className="md:w-6/12 min-h-60 max-h-80 skeleton bg-[#dddd] rounded"></div>
+                      <div className="md:w-6/12 flex flex-col justify-evenly">
+                        <div className="flex gap-3">
+                          <Skeleton circle className="!w-10 h-10" />
+                          <div className="">
+                            <Skeleton className="!rounded-full !w-60 h-5" />
+                            <Skeleton className="!w-20 h-2" />
+                          </div>
+                        </div>
+                        <div className="space-y-1 text-gray-600 [&>div>span]:text-sm md:[&>div>span]:text-base [&>div>span]:font-semibold">
+                          <Skeleton className="!rounded-full !w-4/12 h-3" />
+                          <Skeleton className="!rounded-full !w-3/12 h-3" />
+                          <Skeleton className="!rounded-full !w-5/12 h-3" />
+                          <Skeleton className="!rounded-full !w-2/12 h-3" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="py-5 space-y-3">
+                      <Skeleton className="!rounded-full !w-12/12 h-8 mb-5" />
+                      <Skeleton className="!rounded-full !w-7/12 h-3" />
+                      <Skeleton className="!rounded-full !w-9/12 h-3" />
+                      <Skeleton className="!rounded-full !w-5/12 h-3" />
+                      <Skeleton className="!rounded-full !w-11/12 h-3" />
+                      <Skeleton className="!rounded-full !w-8/12 h-3" />
+                    </div>
+                  </article>
                 </>
               ) : (
-                <div className="">
+                <article className="">
                   <div className="flex flex-col md:flex-row gap-5">
                     <figure className="md:w-6/12 max-h-80 skeleton bg-[#dddd]  rounded flex justify-center">
                       <img
@@ -85,19 +113,27 @@ const PostDetails = () => {
                                 <>{`${Math.round(days)} day${
                                   Math.round(days) !== 1 ? "s" : ""
                                 } ago`}</>
-                              )) || (
+                              )) ||
+                              (weeks < 52 && (
                                 <>{`${Math.round(weeks)} week${
                                   Math.round(weeks) !== 1 ? "s" : ""
+                                } ago`}</>
+                              )) || (
+                                <>{`${Math.round(years)} year${
+                                  Math.round(years) !== 1 ? "s" : ""
                                 } ago`}</>
                               )}
                           </div>
                         </div>
                       </div>
-                      <div className="space-y-1 text-gray-600 [&>div>span]:text-base [&>div>span]:font-semibold">
-                        <div className="flex items-center gap-3">
-                          <MdOutlineBloodtype className="text-xl" />
-                          <span>{data?.blood_group}</span>
-                        </div>
+                      <div className="space-y-1 text-gray-600 [&>div>span]:text-sm md:[&>div>span]:text-base [&>div>span]:font-semibold">
+                        {data?.post_type !== "event" && (
+                          <div className="flex items-center gap-3">
+                            <MdOutlineBloodtype className="text-xl" />
+                            <span>{data?.blood_group}</span>
+                          </div>
+                        )}
+
                         {data?.post_type && (
                           <div className="flex items-center gap-3 capitalize">
                             <TbUrgent className="text-xl" />
@@ -123,7 +159,7 @@ const PostDetails = () => {
                         <div className="flex items-start gap-3">
                           <HiOutlineLocationMarker className="text-xl mt-1" />
                           <div>
-                            <span className="text-base font-semibold">
+                            <span className="text-sm md:text-base font-semibold">
                               {data?.hospital_address}
                             </span>
                             <span className="block capitalize text-sm">
@@ -135,7 +171,15 @@ const PostDetails = () => {
                       </div>
                     </div>
                   </div>
-                </div>
+                  <div className="py-5 space-y-3">
+                    <h1 className="text-xl md:text-3xl font-bold">
+                      {data?.title}
+                    </h1>
+                    <p className="text-lg font-medium text-gray-700">
+                      {data?.description}
+                    </p>
+                  </div>
+                </article>
               )}
             </>
           ) : (
