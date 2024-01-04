6bc88c3bdf018e674f1bd71557c8aa48fc413648
DeveloperMahTab <mahtabhossain1014@gmail.com>
2024-01-04 15:53:15 +0600

    call to action in blood donors page

--- src/pages/BloodDonors/BloodDonors.jsx
+++ src/pages/BloodDonors/BloodDonors.jsx
@@ -8,6 +8,30 @@ const BloodDonors = () => {
   return (
     <>
       <DomHead title="Blood Donors" />
+
+      <section className="bg-slate-200">
+        <Container>
+          <div className="hero">
+            <div className="hero-content flex-col lg:flex-row">
+              <img src={Kids} className="md:max-w-md rounded-lg shadow-2xl" />
+              <div>
+                <h2 className="text-xl md:text-3xl font-bold">
+                  Join the Lifesaving Team:{" "}
+                  <span className="text-blue-600">Be A Blood Donor!</span> 🩸
+                </h2>
+                <p className="py-6 font-medium">
+                  Every drop counts, and so does your compassion. Take a moment
+                  to make a difference <br /> — become a blood donor today. Your
+                  act of kindness can save lives and bring hope to those in
+                  need.
+                </p>
+                <Button variant="contained">Join Now</Button>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </section>
+
       <section className="py-10 bg-white">
         <Container>
           <div></div>
